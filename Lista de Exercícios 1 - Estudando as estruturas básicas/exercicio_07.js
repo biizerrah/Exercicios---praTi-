c090dba9e8d7cb2912fe10a3e3767d393351fab2
biizerrah <biizerrah@outlook.com>
2025-05-02 21:39:40 -0300

    Exercicio 07 finalizado

--- Lista de Exercícios 1 - Estudando as estruturas básicas/exercicio_07.js
+++ Lista de Exercícios 1 - Estudando as estruturas básicas/exercicio_07.js
@@ -1,3 +1,12 @@
-/*7. As maçãs custam R$ 0,30 se forem compradas menos do que uma dúzia, e R$ 0,25 se
-forem compradas pelo menos doze. Escreva um algoritmo que leia o número de maçãs
-compradas, calcule e escreva o valor total da compra.*/
+const prompt = require('prompt-sync')()
+
+
+
+let maca = Number(prompt("Quantas maçãs deseja levar? "))
+
+let preco = maca >= 12 ? 0.25 : 0.30
+let total = maca * preco
+
+console.log('====================================')
+console.log(`O total da sua compra é: ${total.toFixed(2)}`)
+console.log('====================================')
